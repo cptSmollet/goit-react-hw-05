95d5f71e4d9418b3827aeb8c44a9eed1d0eff3f7
cptSmollet <150056647+cptSmollet@users.noreply.github.com>
2024-09-09 16:08:46 +0200

    fixing remarks4 movieslist(useLocation)

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import css from './MovieList.module.css';
 
 const MovieList = ({ movies }) => {
+  const location = useLocation(); 
+
   return (
     <ul className={css.movieList}>
       {movies.map(movie => (
         <li key={movie.id} className={css.movieItem}>
-          <Link to={`/movies/${movie.id}`}>
+          {/* Передаємо поточне місце як state до кожного Link */}
+          <Link 
+            to={`/movies/${movie.id}`} 
+            state={{ from: location }}
+          >
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
